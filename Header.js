674eb006e00d907888f9574be82f0f20e0ff73a4
Igor Matheus <silvalimaigormatheus@gmail.com>
2023-02-28 19:06:09 -0300

    tentando estilizar

--- Header.js
+++ Header.js
@@ -1,15 +1,18 @@
 import React, {useContext} from 'react';
 import { ThemeContext } from './App';
 import Button from "./button";
+import './Header.css';
 export default function Header(props) {
   const {onToggleTheme }= useContext(ThemeContext)
+  console.log("tittle1")
  return (
    <>
-   <h1 >{props.title}</h1>
+
+    <div className="tittle1"><h1>{props.title}</h1>
     {props.children}
     <Button onClick={onToggleTheme}>
         Mudar Tema
-    </Button>
+    </Button></div>
    </>
   );
 }
